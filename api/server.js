d7728bf6c2e64794934c95571d3f79f6b19a618a
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-07-15 18:04:13 +0300

    proje set-up

---
+++ api/server.js
@@ -0,0 +1,26 @@
+//1-IMPORT
+
+
+const express = require("express");
+//express kullanarak server instance yarat
+const server = express();
+
+
+
+//2-GLOBAL MIDDLEWARE
+
+
+
+//3-ROUTER
+server.get("/", (req,res) =>{
+    res.json({message:"Server up and running..."})
+})
+
+
+
+//4-ERROR MIDDLEWARE
+
+
+//5-EXPORT
+
+module.exports= server
