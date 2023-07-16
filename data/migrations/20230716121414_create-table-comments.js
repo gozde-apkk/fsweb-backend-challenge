ff8a6e7efb4ec7bd0b1d8df260eef0a91b51f0fc
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-07-16 18:10:44 +0300

    comments-migration created

--- data/migrations/20230716121414_create-table-comments.js
+++ data/migrations/20230716121414_create-table-comments.js
@@ -3,7 +3,25 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('Comments', tbl =>{
+    tbl.increments('comment_id').primary()
+    tbl.string('content')
+    tbl.timestamp('created_at').defaultTo(knex.fn.now())
+    tbl.integer('user_id')
+        .notNullable()
+        .references('user_id')
+        .inTable('Users')
+        .unsigned()
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    tbl.integer('tweet_id')
+        .notNullable()
+       .references('tweet_id')
+       .inTable('tweets')
+       .unsigned()
+       .onDelete('CASCADE')
+       .onUpdate('CASCADE')
+  })
 };
 
 /**
@@ -11,5 +29,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropSchemaIfExists('comments');
 };
