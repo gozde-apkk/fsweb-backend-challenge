030ba21479c50156cf715ef22acf14bbdc35c3a0
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-07-17 15:00:24 +0300

    user-seed created

---
+++ data/seeds/001-users.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('users').truncate()
+  await knex('users').insert([
+    {user_id: 1,
+       username: 'shxdxx',
+       name:'gozde',
+       password:'1234',
+       email:'shxdxx@gmail.com',
+       created_at: new Date()},
+    {user_id: 2, 
+      username: 'master',
+      name:'ege',
+      password:'1234',
+      email:'master@gmail.com',
+      created_at: new Date(),},
+    {user_id: 3,
+       username: 'developer',
+       name:'ilke',
+       password:'1234',
+       email:'develop@gmail.com',
+       created_at: new Date(),}
+  ]);
+};
