3403d96961c0a6f0d523ba62a7d1cc2083c3fe32
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-07-17 17:55:43 +0300

    user-middleware

--- api/User/-middleware.js
+++ api/User/-middleware.js
@@ -1,7 +1,33 @@
+const UserModel = require('./-model');
 
 
-const  restricted = (req,res,next) =>{
+async function validateUserId(req, res, next) {
+    
+    try{
+     const user = await UserModel.getById(req.params.id);
+      if(user){
+        req.user = user;
+        next();
+        
+      }else{
+        res.status(400).json({message: `${id} id'li kullanıcı bulunamadı!...`})
+      }
+    }catch(error){
+      next(error);
+    }
+  }
 
+async function validateLogin(req,res,next){
+    try{
+        let existUser = await UserModel.findUser(req.body.username, req.body.password);
+        if(!existUser){
+            res.status(404).json({
+                message:"Böyle bir kullanıcı yok"
+            })
+        }
+    }catch(err){
+        next(err);
+    }
 }
 
-module.exports ={restricted}
+module.exports ={validateUserId, validateLogin}
