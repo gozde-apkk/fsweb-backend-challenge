254e997d53c601d83cb10ce0061746724e2ab1c0
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-07-17 17:25:42 +0300

    Users-model

--- api/User/-model.js
+++ api/User/-model.js
@@ -1,7 +1,29 @@
 
-
-const getAll = () =>{
-
+const db =require('../../data/db-config');
+//kullanıcıyı veritabanından al
+function getAll  () {
+   return db('Users');
 }
+//idil ebul
+function getById (id) {
+    return db('Users').where({user_id: user_id }).first();
+}
+//filtreleyerek ara
+function getByFilter(filter) {
+    return db('users').where(filter);
+  }
 
-module.exports = {getAll}
+  //kullanıcıyı sil
+  async function removeUser(id) {
+    return await db('users').where("user_id", id).delete();
+};
+//kullanıcı ekle
+async function insertUser(newUser) {
+    await db("users").insert(newUser);
+};
+//kullanıcıyı güncellet
+async function updateUser(id, payload) {
+    return await db('users').where("user_id", id).update(payload);
+};
+
+module.exports = {getAll, getById , getByFilter,removeUser,updateUser,insertUser}
